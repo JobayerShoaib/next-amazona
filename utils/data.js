20e262aedc3d5a89cf70788390800458b94f12e0
JobayerShoaib <jobayershoaib@gmail.com>
2022-11-17 16:33:18 +0600

    updated

---
+++ utils/data.js
@@ -0,0 +1,72 @@
+const data={
+    products:[
+        {
+            name:'Free Shirt',
+            slug:'free-shirt',
+            category:'Shirts',
+            image:'/images/shirt1.jpg',
+            price:70,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:8,
+            countInStock:20,
+            description:'A popular shirt'
+        },{
+            name:'Fit Shirt',
+            slug:'fit-shirt',
+            category:'Shirts',
+            image:'/images/shirt2.jpg',
+            price:80,
+            brand:'Adidas',
+            rating:3.2,
+            numReviews:10,
+            countInStock:20,
+            description:'A popular shirt'
+        },{
+            name:'Slim Shirt',
+            slug:'slim-shirt',
+            category:'Shirts',
+            image:'/images/shirt3.jpg',
+            price:90,
+            brand:'Raymond',
+            rating:3.2,
+            numReviews:10,
+            countInStock:20,
+            description:'A popular shirt'
+        },{
+            name:'Golf-Pants',
+            slug:'golf-pants',
+            category:'Pants',
+            image:'/images/pants1.jpg',
+            price:95,
+            brand:'Raymond',
+            rating:3.2,
+            numReviews:12,
+            countInStock:20,
+            description:'A popular pant'
+        },{
+            name:'Classic-Pants',
+            slug:'classic-pants',
+            category:'Pants',
+            image:'/images/pants2.jpg',
+            price:85,
+            brand:'Raymond',
+            rating:4.2,
+            numReviews:18,
+            countInStock:20,
+            description:'A popular pant'
+        },{
+            name:'Slim-Pants',
+            slug:'slim-pants',
+            category:'Pants',
+            image:'/images/pants3.jpg',
+            price:60,
+            brand:'Raymond',
+            rating:3.6,
+            numReviews:18,
+            countInStock:20,
+            description:'A popular pant'
+        }
+    ]
+}
+export default data
